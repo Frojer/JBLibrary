eb0c93dd6d8d9a4e0e64e78da49ad6fadd852ef0
MKzz <ltw94@hotmail.com>
2015-03-14 11:02:52 +0900

    custom vector class modified

--- Game/JBLibrary/Global/JBGLBCustomVector3.hpp
+++ Game/JBLibrary/Global/JBGLBCustomVector3.hpp
@@ -18,6 +18,8 @@ namespace JBL{
             customVector3(const _FLOAT _x, const _FLOAT _y, const _FLOAT _z) :x(_x), y(_y), z(_z){}
             customVector3(const customVector3& v) :x(v.x), y(v.y), z(v.z){}
         public:
+            inline customVector3& operator=(const customVector3& v){ x = v.x; y = v.y; z = v.z; }
+        public:
             inline bool operator!()const{ return !x && !y && !z; }
             inline bool operator==(const customVector3& v)const{ return x == v.x && y == v.y && z == v.z; }
             inline bool operator!=(const customVector3& v)const{ return x != v.x && y != v.y && z != v.z; }
@@ -33,14 +35,30 @@ namespace JBL{
             inline customVector3 operator-(const customVector3& v)const{ return customVector3(*this) -= v; }
         public:
             /// @brief 벡터의 길이(크기)를 구합니다.
-            /// @return 실수 값
-            inline _FLOAT length()const{ return sqrt((x ^ 2) + (y ^ 2) + (z ^ 2)); }
+            /// @return 연산 결과
+            inline _FLOAT length()const{ return _FLOAT(sqrt((x * x) + (y * y) + (z * z))); }
 
             /// @brief 단위 벡터를 구합니다.
-            /// @return 벡터 값
+            /// @return 연산 결과
             inline customVector3 normalize()const{
                 _FLOAT len(length());
                 return !len ? customVector3(0.f, 0.f, 0.f) : customVector3(*this) /= len;
+            }
+        public:
+            /// @brief 대상 벡터와의 내적을 구합니다.
+            /// @param v 연산 대상
+            /// @return 연산 결과
+            inline _FLOAT dot(const customVector3& v)const{ return _FLOAT((x * v.x) + (y * v.y) + (z * v.z)); }
+
+            /// @brief 대상 벡터와의 외적을 구합니다.
+            /// @param v 연산 대상
+            /// @return 연산 결과
+            inline customVector3 cross(const customVector3& v)const{
+                return customVector3(
+                    (y * v.z) - (z * v.y),
+                    (z * v.x) - (x * v.z),
+                    (x * v.y) - (y * v.x)
+                    );
             }
         };
     };
